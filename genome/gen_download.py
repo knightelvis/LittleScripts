2a702794f709e32636070b1df7a1bfb08c79af6d
knightelvis <archerlaus@gmail.com>
2020-02-23 21:47:16 -0800

    update script to download low coverage, exome, high coverage separately

--- genome/gen_download.py
+++ genome/gen_download.py
@@ -15,12 +15,8 @@ with FTP('ftp.1000genomes.ebi.ac.uk') as ftp:
     race_files = ftp.nlst() # files is ['ACB', 'ASW', 'BEB', 'CDX', 'CEU', 'CHB', 'CHS', 'CLM', 'ESN', 'FIN', 'GBR', 'GIH', 'GWD', 'IBS', 'ITU', 'JPT', 'KHV', 'LWK', 'MSL', 'MXL', 'PEL', 'PJL', 'PUR', 'STU', 'TSI', 'YRI']
     print("Total races:" + ",".join(race_files))
 
-    with open('output.txt', 'w') as f:
+    with open('output_low.txt', 'w') as f_low, open('output_exo.txt', 'w') as f_exo, open('output_high.txt', 'w') as f_hig:
         for r in race_files:
-            # TODO remove
-            if r == 'ACB':
-                continue
-
             second_level_path = os.path.join(base_path,r)
             ftp.cwd(second_level_path) # currently in 'data/ACB/'
             individual_files = ftp.nlst() # ['HG01879'...]
@@ -29,15 +25,40 @@ with FTP('ftp.1000genomes.ebi.ac.uk') as ftp:
                 third_level_path = os.path.join(second_level_path, i)
                 ftp.cwd(third_level_path)
                 alignments = ftp.nlst()
-                if not 'alignment' in alignments:
-                    continue
-                third_level_path = os.path.join(third_level_path,'alignment')
-                target_files = ftp.nlst(third_level_path) # *.bas, *.cram ...
+                for a in alignments:
+                    print(a)
+                    if a == 'alignment':
+                        fourth_level_path = os.path.join(third_level_path, a)
+                        target_files = ftp.nlst(fourth_level_path) # *.bas, *.cram ...
 
-                for t in target_files:
-                    if t.endswith('.cram'):
-                        output_file = os.path.join(third_level_path,t)
-                        line = prefix + output_file + '\n'
-                        f.write(line)
-                        print(line)
+                        for t in target_files:
+                            if t.endswith('.cram'):
+                                output_file = os.path.join(fourth_level_path, t)
+                                line = prefix + output_file + '\n'
+                                f_low.write(line)
+                                print(line)
+
+                    elif a == 'exome_alignment':
+                        fourth_level_path = os.path.join(third_level_path, a)
+                        target_files = ftp.nlst(fourth_level_path) # *.bas, *.cram ...
+
+                        for t in target_files:
+                            if t.endswith('.cram'):
+                                output_file = os.path.join(fourth_level_path, t)
+                                line = prefix + output_file + '\n'
+                                f_exo.write(line)
+                                print(line)
+
+                    elif a == 'high_cov_alignment':
+                        fourth_level_path = os.path.join(third_level_path, a)
+                        target_files = ftp.nlst(fourth_level_path) # *.bas, *.cram ...
+
+                        for t in target_files:
+                            if t.endswith('.cram'):
+                                output_file = os.path.join(fourth_level_path, t)
+                                line = prefix + output_file + '\n'
+                                f_hig.write(line)
+                                print(line)
+                    else:
+                        print("###" + a)
 
